1765070fcde2c7571ca56f9f828552bd1c0e0094
kekkaerika <94744638+kekkaerika@users.noreply.github.com>
2023-09-15 20:54:54 +0200

    Update ConPool.java

--- ecommerceCar/src/main/java/model/dao/ConPool.java
+++ ecommerceCar/src/main/java/model/dao/ConPool.java
@@ -6,7 +6,7 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public class ConPool {
-
+	private ConPool(){}
 	 private static DataSource datasource;
 
 	    public static Connection getConnection() throws SQLException {
