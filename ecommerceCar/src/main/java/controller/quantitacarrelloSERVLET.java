601a6ec2716f679f86c523c89e70f2e478c0aea4
Samuele Sparno <94744859+Sewaaa@users.noreply.github.com>
2023-09-18 15:53:59 +0200

    Update quantitacarrelloSERVLET.java

--- ecommerceCar/src/main/java/controller/quantitacarrelloSERVLET.java
+++ ecommerceCar/src/main/java/controller/quantitacarrelloSERVLET.java
@@ -73,7 +73,7 @@ public class quantitacarrelloSERVLET extends HttpServlet {
 			
 			/*AGGIUNGI AL CARRELLO GESTIONE DALLA SESSIONE*/
 			if(email == null || email.isEmpty()) {
-				ArrayList<prodotto> s = new ArrayList<>();
+				ArrayList<prodotto> s ;
 				s = (ArrayList<prodotto>) sessione.getAttribute(idProdotto);
 				int n = Integer.parseInt(request.getParameter("posizione"));
 					if(s.get(n).getQuantita() == 1) {
